d20fe346e79a93a2c5b2fcb88ef0f667a66d6e91
enadol <enadol@yahoo.com>
2024-10-12 16:15:46 +0000

    modified:   players.py 	modified:   requirements.txt

--- players.py
+++ players.py
@@ -3,4 +3,9 @@ import pandas as pd
 import openpyxl
 
 # read file bayern_players.xlsx
+df = pd.read_excel('bayern_players.xlsx')
+print(df.head())
 
+print(df.columns)
+
+# print data for Harry Kane
